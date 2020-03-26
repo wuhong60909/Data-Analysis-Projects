b48a8cf3d19d322312c50a834bcf98b09b60a31a
wuhong60909 <wuhong60909@gmail.com>
2020-03-26 12:47:27 +0800

    Modify input: y_true and y_score.

--- Credit_Card_Fraud_Detection/curves.py
+++ Credit_Card_Fraud_Detection/curves.py
@@ -6,15 +6,12 @@ classification_report, confusion_matrix, precision_recall_curve, \
 average_precision_score, roc_curve, auc
 
 class curves:
-    def __init__(self, clf, X, y):
+    def __init__(self, y_true, y_score):
         '''
         Binary label case only!
         '''
-        self.clf = clf
-        self.X = X
-        self.y = y
-        self.test_score = pd.DataFrame({'True_class': self.y, 
-                                        'score': clf.predict_proba(self.X)[:, 1]})
+        self.test_score = pd.DataFrame({'True_class': y_true, 
+                                        'score': y_score})
         # ROC curve
         self.fpr, self.tpr, _ = roc_curve(self.test_score.True_class, self.test_score.score)
         self.roc_auc = auc(self.fpr, self.tpr)
@@ -35,20 +32,20 @@ class curves:
                     s = 5, label = '1')
         plt.xlabel('Index')
         plt.ylabel('Score')
-        plt.title('Classifier: ' + self.clf.__class__.__name__)
+        plt.title('Score scatter plot')
         plt.legend()
         plt.show()
 
     def ROC_plot(self):     
-        plt.figure(figsize = (8, 5))
+        plt.figure(figsize = (12, 5))
         plt.plot(self.fpr, self.tpr, linewidth = 3, label = 'AUC = %0.3f' % (self.roc_auc))
         plt.plot([0, 1], [0, 1], linewidth = 3)
         plt.xlim(left = -0.02, right = 1)
         plt.ylim(bottom = 0, top = 1.02)
         plt.xlabel('False Positive Rate (FPR)')
         plt.ylabel('True Positive Rate (TPR)')
-        plt.title('Receiver operating characteristic curve ({})'.format(self.clf.__class__.__name__))
-        plt.legend()
+        plt.title('Receiver operating characteristic curve (ROC)')
+        plt.legend(loc = 'lower right')
         plt.show()
 
     def PRF_plot(self):
@@ -56,7 +53,7 @@ class curves:
         max_f1 = self.f1_scores.max()
         best_threshold = self.threshold[self.f1_scores[1: ] == max_f1]
 
-        plt.figure(figsize = (12, 6))
+        plt.figure(figsize = (12, 5))
         plt.plot(self.threshold, self.precision[1: ], label = "Precision", linewidth = 3)
         plt.plot(self.threshold, self.recall[1: ], label = "Recall", linewidth = 3)
         plt.plot(self.threshold, self.f1_scores[1: ], label = "F1 score", linewidth = 3, color = 'green')
@@ -65,13 +62,13 @@ class curves:
         plt.ylim(0, 1.1)
         plt.xlabel('Threshold')
         plt.ylabel('Precision/ Recall/ F1 score')
-        plt.title('Precision and recall for different threshold values ({})'.format(self.clf.__class__.__name__))
+        plt.title('Precision, recall and F1 score for different threshold values')
         plt.legend(loc = 'upper right')
         plt.show()
 
     def PR_plot(self):
         # Precision - Recall curve
-        plt.figure(figsize = (12, 6))
+        plt.figure(figsize = (12, 5))
         f_scores = np.linspace(0.2, 0.8, num = 4)
         for f_score in f_scores:
             x = np.linspace(0.001, 1)
@@ -85,6 +82,6 @@ class curves:
         plt.ylim(0, 1.1)
         plt.xlabel('Recall')
         plt.ylabel('Precision')
-        plt.title('Precision - Recall curve ({})'.format(self.clf.__class__.__name__))
+        plt.title('Precision - Recall curve')
         plt.legend(loc = 'upper right')
         plt.show()
